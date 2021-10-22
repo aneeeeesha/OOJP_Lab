4d8509a77ad85909682fd19d5c485cf5cacedf58
aneeeeesha <84973812+aneeeeesha@users.noreply.github.com>
2021-10-22 12:54:13 +0530

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Switchcase{
+
+public static void main(String args[])
+{
+ int num;
+ Scanner in=new Scanner(System.in);
+ num=in.nextInt();
+ switch(num)
+ {
+ case 1:
+   System.out.println("Monday");
+break;
+ case 2:
+   System.out.println("Tuesday");
+
+}
+
+
+
+}
+
+
+}
