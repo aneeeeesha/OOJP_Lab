beac40d99b2c6f6236676fb96be6801108301f41
aneeeeesha <84973812+aneeeeesha@users.noreply.github.com>
2021-10-29 12:44:06 +0530

    Usage of array
    This has arrays in it

--- Marks.java
+++ Marks.java
@@ -6,14 +6,22 @@ class Marks{
 public static void main(String args[])
 {
 
+int subjects,number;
 Scanner in =new Scanner(System.in);
 
+System.out.println("Enter the number of subjects whose grades you want to calculate");
+subjects=in.nextInt();
+char grades[]=new char[subjects];
+
+
+for(number=0;number<subjects;number++)
+{
 int cie,see;
 while(true)
 {
 System.out.println("Enter your CIE marks");
  cie = in.nextInt();
- if(cie<0 || cie>=50)
+ if(cie<0 || cie>50)
 	System.out.println("Entered marks is invalid");
  else
    break;
@@ -23,28 +31,35 @@ while(true)
 {
 System.out.println("Enter your SEE marks");
  see = in.nextInt();
- if(see<0 || see>=100)
+ if(see<0 || see>100)
 	System.out.println("Entered marks is invalid");
  else
    break;
 
 }
-System.out.println("The entered marks \n CIE: "+cie+"\n SEE: "+see); 	
+	
 int total=cie+(see/2);
 if(total>=90)
-	System.out.println("Grade obtained in this subject is S");
+	grades[number]='S';
 else if(total<90 && total >=80)
-	System.out.println("Grade obtained in this subject is A");
+	grades[number]='A';
 else if(total<80 && total >=70)
-	System.out.println("Grade obtained in this subject is B");
+	grades[number]='B';
 else if(total<70 && total >=60)
-	System.out.println("Grade obtained in this subject is C");
+	grades[number]='C';
 else if(total<60 && total >=50)
-	System.out.println("Grade obtained in this subject is D");
+	grades[number]='D';
 else if(total<50 && total >=40)
-	System.out.println("Grade obtained in this subject is E");
+	grades[number]='E';
 else if(total<40)
-	System.out.println("Grade obtained in this subject is F");
+	grades[number]='F';
 
+
+
+
+
+}
+for(int k=0;k<number;k++)
+ System.out.println("The grade in subject "+(k+1)+" "+grades[k]);
 }
 }
