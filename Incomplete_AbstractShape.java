77d4a76cfbbf3ab33b714d2e803419d81bc76d3c
aneeeeesha <84973812+aneeeeesha@users.noreply.github.com>
2021-12-10 12:14:07 +0530

    Update Incomplete_AbstractShape.java

--- Incomplete_AbstractShape.java
+++ Incomplete_AbstractShape.java
@@ -7,7 +7,35 @@ int b;
 Shape(int a,int b){
 this.a=a;
 this.b=b;}
-abstract int printArea();
+abstract void printArea();
+
+
+Shape(int a){
+this.a=a;}
+
+}
+
+class Triangle extends Shape{
+Triangle(int a,int b){
+super(a,b);
+
+}
+void printArea(){
+System.out.println("The given triangle's area is as follows : "+(0.5*a*b));
+}
+}
+
+class Circle extends Shape{
+Circle(int a){
+super(a);
+
+}
+
+void printArea(){
+
+System.out.println("The area of the circle is :"+(3.14*a*a));
+}
+
 
 }
 
@@ -16,25 +44,48 @@ Rectangle(int a,int b)
 {
 super(a,b);
 }
-int printArea(){
-System.out.println("Enered   ");
-return 0;
-}
+void printArea(){
+System.out.println("The entered shape's area is : "+(a*b));
 
 }
 
-class AbstractShape{
+}
+
+class Abstract_Shape{
 
 
 public static void main(String args[])
 {
-System.out.println("Enter the dimensions of the shape");
-Scanner in = new Scanner(System.in);
-int d1=in.nextInt();
-int d2=in.nextInt();
+int d1,d2;
 
-Rectangle r=new Rectangle(d1,d2);
-r.printArea();
+System.out.println("Enter the shape taht you want to find the area of ");
+System.out.println("\nPress 1 for Rectangle \nPress2 for Triangle \n Press 3 for Circle\n");
+
+Scanner in = new Scanner(System.in);
+int type=in.nextInt();
+System.out.println("Enter the dimensions of the shape");
+switch(type){
+case 1:
+	d1=in.nextInt();
+	d2=in.nextInt();
+	Rectangle r=new Rectangle(d1,d2);
+	r.printArea();
+case 2:
+	d1=in.nextInt();
+	d2=in.nextInt();
+	Triangle t=new Triangle(d1,d2);
+	t.printArea();
+case 3:
+	d1=in.nextInt();
+	Circle c=new Circle(d1);
+	c.printArea();
+	
+
+}
+
+
+
+
 
 
 }
